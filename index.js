cf5ee8ee579b1b0d50fb2d5bfe859efa56a50b9a
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-04-03 00:13:31 +0530

    Add cors middleware to enable Cross-Origin Resource Sharing (CORS)

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const Router = require("./routes/routes.js");
+const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 const port = process.env.PORT;
 
@@ -11,8 +14,8 @@ app.get("/", (req, res) => {
     res.send("Hello");
 });
 
-app.use("/",Router);
+app.use("/", Router);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Server started at port ${port}`);
 })
