53713057d278aa09c598fca288366f797ea88d78
Rajat4279 <rajatsrivastava4279@gmail.com>
2024-07-07 21:54:07 +0530

    Refactor Google Translation API initialization and error handling

--- googleAPI/googleTranslationAPI.js
+++ googleAPI/googleTranslationAPI.js
@@ -3,8 +3,8 @@ const { Translate } = require('@google-cloud/translate').v2;
 const API = async (credentials, text, target) => {
 
     const translate = new Translate({
-        credentials: JSON.parse(credentials),
-        projectId: JSON.parse(credentials).project_id
+        credentials: credentials,
+        projectId: credentials.project_id
     });
     
     return translatorFunction(translate, text, target);
